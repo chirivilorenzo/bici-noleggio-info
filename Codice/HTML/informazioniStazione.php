5e55c8461584a0337e23e6d86c6ca6c11df2a54e
chirivilorenzo <chirivilorenzo@gmail.com>
2024-05-14 12:34:10 +0200

    aggiunto sito al click di una stazione

---
+++ Codice/HTML/informazioniStazione.php
@@ -0,0 +1,41 @@
+<?php
+
+include("../PHP/classi/CDatabase.php");
+
+if(isset($_GET["id"])){
+    $id = $_GET["id"];
+
+    //visualizza tutte le info di quella stazione
+    $classeDB = new CDatabase();
+    $classeDB->connessione();
+
+    $query = "SELECT * FROM stazione WHERE ID = ?";
+    $result = $classeDB->seleziona($query, $id);
+
+    if($result != "errore" && $result != "vuoto"){
+        $stringa = "        <table>
+        <th>Nome</th>
+        <th>Latitudine</th>
+        <th>Longitudine</th>
+        <th>bici disponibili</th>
+        <th>slot totali</th><tr>";
+        $stringa .= "<td>" . $result[0]["nome"] . "</td>";
+        $stringa .= "<td>" . $result[0]["latitudine"] . "</td>";
+        $stringa .= "<td>" . $result[0]["longitudine"] . "</td>";
+        $stringa .= "<td>" . $result[0]["num_bici_disp"] . "</td>";
+        $stringa .= "<td>" . $result[0]["num_slot_tot"] . "</td>";
+        $stringa .= "</tr></table>";
+        echo $stringa;
+    }
+}
+else{
+    echo "parametro get non valido o non trovato";
+}
+?>
+<html>
+    <head>
+    </head>
+    <body>
+        <a href="mappa.html">Indietro</a>
+    </body>
+</html>
